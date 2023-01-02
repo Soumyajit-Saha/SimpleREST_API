25102dee5bac8a52ac48859d85e9bfd357e8e344
unknown <soumyajitsaha1997@gmail.com>
2023-01-02 02:28:45 -0700

    added the REST API

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import userRoutes from './routes/users.js';
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.use('/users' , userRoutes);
+
+app.get('/', (req, res) =>{
+    // console.log('TEST');
+    res.send('Hello from Home Page!')
+});
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
